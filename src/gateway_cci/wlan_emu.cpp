2add88ad632efb3f534c9ad54291bdac75918b46
rranga540 <RaviShankar.R_Rangaraj@comcast.com>
2026-02-18 16:03:22 +0000

    Add additional changes for multiclient feature

--- src/gateway_cci/wlan_emu.cpp
+++ src/gateway_cci/wlan_emu.cpp
@@ -616,6 +616,10 @@ int wlan_emu_t::init()
 
     m_ext_sta_mgr.ext_agent_iface.m_ui_mgr = &m_ui_mgr;
 
+    wlan_emu_print(wlan_emu_log_level_info, "%s:%d: Simulated client count set to %d\n", __func__,
+        __LINE__, sim_client_count);
+    wlan_emu_print(wlan_emu_log_level_info,
+        "%s:%d: size of if_map returned by wifi_hal_getInterfaceMap: %d\n", __func__, __LINE__, if_map_size);
     if (m_sim_sta_mgr.init(if_map, if_map_size) == RETURN_ERR) {
         wlan_emu_print(wlan_emu_log_level_err, "%s:%d: sta_mgr init failed\n", __func__, __LINE__);
         free(if_map);
