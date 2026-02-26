f8e5f316fa1d763f72bdc074d130d6dfa9aee98a
Pavithra Sundaravadivel <Pavithra_Sundaravadivel@Comcast.com>
2026-02-26 07:36:55 +0000

    RDKB-36610 Optimize PR0270

--- src/gateway_cci/wlan_emu.cpp
+++ src/gateway_cci/wlan_emu.cpp
@@ -106,12 +106,15 @@ int wlan_emu_t::run()
             // download the test and respective configs will be downloaded
             wlan_emu_print(wlan_emu_log_level_dbg,
                 "%s:%d: in case wlan_emu_tests_state_cmd_request\n", __func__, __LINE__);
-            if (m_ext_sta_mgr.init() != RETURN_OK) {
-                wlan_emu_print(wlan_emu_log_level_err,
-                    "%s:%d: update_external_agent_capabilities failed\n", __func__, __LINE__);
-                m_ui_mgr.signal_report_test_fail();
-                dml_state = wlan_emu_dml_tests_state_complete_failure;
-                break;
+            wlan_emu_print(wlan_emu_log_level_dbg, "%s:%d tda_url is %s\n", __func__, __LINE__, m_ui_mgr.get_tda_url());
+            if (strstr(m_ui_mgr.get_tda_url(), "PR0270") != NULL) {
+                if (m_ext_sta_mgr.init() != RETURN_OK) {
+                    wlan_emu_print(wlan_emu_log_level_err,
+                        "%s:%d: update_external_agent_capabilities failed\n", __func__, __LINE__);
+                    m_ui_mgr.signal_report_test_fail();
+                    dml_state = wlan_emu_dml_tests_state_complete_failure;
+                    break;
+                }
             }
 
             if (m_ui_mgr.analyze_request() == RETURN_OK) {
