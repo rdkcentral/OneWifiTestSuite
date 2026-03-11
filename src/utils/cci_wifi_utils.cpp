98875743de4a57d624d86d6a83de41913e828aec
Pavithra Sundaravadivel <Pavithra_Sundaravadivel@Comcast.com>
2026-03-11 14:27:39 +0000

    REAL client

--- src/utils/cci_wifi_utils.cpp
+++ src/utils/cci_wifi_utils.cpp
@@ -1347,20 +1347,19 @@ int is_valid_ip(const char *addr)
     struct sockaddr_in sa;
     struct sockaddr_in6 sa6;
 
-    if (inet_pton(AF_INET, addr, &(sa.sin_addr)) <= 0) {
-        wlan_emu_print(wlan_emu_log_level_err, "%s:%d: unable to resolve ipv4 addr: %s\n", __func__,
-            __LINE__, addr);
-        return RETURN_ERR;
+    // Try IPv4
+    if (inet_pton(AF_INET, addr, &(sa.sin_addr)) > 0) {
+        return RETURN_OK;
     }
 
     // Try IPv6
-    if (inet_pton(AF_INET6, addr, &(sa6.sin6_addr)) <= 0) {
-        wlan_emu_print(wlan_emu_log_level_err, "%s:%d: unable to resolve ipv6 addr: %s\n", __func__,
-            __LINE__, addr);
-        return RETURN_ERR;
+    if (inet_pton(AF_INET6, addr, &(sa6.sin6_addr)) > 0) {
+        return RETURN_OK;
     }
 
-    return RETURN_OK;
+    wlan_emu_print(wlan_emu_log_level_err, "%s:%d: unable to resolve addr: %s\n", __func__,
+        __LINE__, addr);
+    return RETURN_ERR;
 }
 
 int is_resolvable_hostname(const char *hostname)
