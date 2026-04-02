59619cb986651d3d47cf77bfdbbce3747489ef83
rranga540 <RaviShankar.R_Rangaraj@comcast.com>
2026-04-02 07:04:57 +0000

    Auto reconnect fix

--- src/tests/wlan_emu_test_param_sta_management.cpp
+++ src/tests/wlan_emu_test_param_sta_management.cpp
@@ -1026,11 +1026,7 @@ int test_step_param_sta_management::step_frame_filter(wlan_emu_msg_t *msg)
                         "%s:%d: Deauth frame received for mac %s, client_macaddr %s for step %d\n",
                         __func__, __LINE__, macaddr, client_macaddr, step->step_number);
                     step->u.sta_test->is_station_associated = false;
-                    if (step->m_sim_sta_mgr->remove_sta(step->u.sta_test) == RETURN_ERR) {
-                        wlan_emu_print(wlan_emu_log_level_err,
-                            "%s:%d: remove_sta failed for step %d\n", __func__, __LINE__,
-                            step->step_number);
-                    }
+                    step->m_sim_sta_mgr->remove_sta(step->u.sta_test);
                     if (step->m_sim_sta_mgr->add_sta(step->u.sta_test) == RETURN_ERR) {
                         wlan_emu_print(wlan_emu_log_level_err,
                             "%s:%d: remove_sta failed for step %d\n", __func__, __LINE__,
