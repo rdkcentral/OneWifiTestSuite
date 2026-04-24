742f4f44b5014418e2d1cbb2d9bf64cf19363045
Pavithra Sundaravadivel <Pavithra_Sundaravadivel@Comcast.com>
2026-04-24 06:27:30 +0000

    RDKB-36610 bpi iter

--- src/tests/wlan_emu_test_param_iperf_server.cpp
+++ src/tests/wlan_emu_test_param_iperf_server.cpp
@@ -171,10 +171,10 @@ int test_step_param_iperf_server::step_timeout()
         wlan_emu_print(wlan_emu_log_level_err, "%s:%d: external agent state failed for %d\n",
             __func__, __LINE__, step->step_number);
         step->test_state = wlan_emu_tests_state_cmd_abort;
-        /*if (ext_agent->send_external_agent_stop_command() != RETURN_OK) {
+        if (ext_agent->send_external_agent_stop_command() != RETURN_OK) {
             wlan_emu_print(wlan_emu_log_level_err,
                 "%s:%d: failed to send external agent stop command\n", __func__, __LINE__);
-        }*/
+        }
         return RETURN_ERR;
     }
 
