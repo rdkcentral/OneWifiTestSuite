ec0eec720528878cf757eddeb119b6a9245aaf0c
pavithrasundaravadivel <pavithra_sundaravadivel@comcast.com>
2026-04-27 11:18:35 +0530

    RDKB-64619 Fix PR0270(BPI connectivity) failure on first iteration (#22)
    Impacted Platforms:
    All RDKB Platforms
    
    Reason for change: Enter into proper namespace for iperf tests
    
    Test Procedure: Run PR0270 testcase and check the status
    
    Risks: None
    Priority: P1
    Signed-off-by:Pavithra_Sundaravadivel@comcast.com

--- src/external_agent_cci/wlan_ext_emu_test_step_param_iperf_client.cpp
+++ src/external_agent_cci/wlan_ext_emu_test_step_param_iperf_client.cpp
@@ -56,10 +56,11 @@ int wlan_ext_test_step_param_iperf_client::wlan_ext_step_execute()
             __func__, __LINE__, step->step_number, iperf_client_cmd.c_str());
 
         if (step->u.iperf_client->interface_type == interface_type_ethernet) {
-            wlan_emu_print(wlan_emu_log_level_dbg, "%s:%d: In ethernet namespace\n", __func__, __LINE__);
-            //list_interfaces_in_namespace();
+            wlan_emu_print(wlan_emu_log_level_dbg, "%s:%d: In ethernet namespace\n", __func__,
+                __LINE__);
+            // list_interfaces_in_namespace();
             ext_emu->enter_namespace("/var/run/netns/ots");
-            //list_interfaces_in_namespace();
+            // list_interfaces_in_namespace();
         }
 
         int result = execute_process_once(iperf_client_cmd,
@@ -68,11 +69,14 @@ int wlan_ext_test_step_param_iperf_client::wlan_ext_step_execute()
             wlan_emu_print(wlan_emu_log_level_err,
                 "%s:%d: step number : %d iperf client cmd : %s failed\n", __func__, __LINE__,
                 step->step_number, step->u.iperf_client->u.start_conf.cmd_options);
+            if (step->u.iperf_client->interface_type == interface_type_ethernet) {
+                ext_emu->leave_namespace();
+            }
             return RETURN_ERR;
         }
         if (step->u.iperf_client->interface_type == interface_type_ethernet) {
             ext_emu->leave_namespace();
-            //list_interfaces_in_namespace();
+            // list_interfaces_in_namespace();
         }
         cJSON_AddItemToArray(step->artifact_json_list,
             cJSON_CreateString(step->u.iperf_client->u.start_conf.result_file));
@@ -94,13 +98,31 @@ int wlan_ext_test_step_param_iperf_client::wlan_ext_step_execute()
         serv_init_step = ext_emu->get_ext_step_from_step_number(
             step->u.iperf_client->u.stop_conf.stop_step_number);
         if (serv_init_step == NULL) {
-            step->step_state = wlan_emu_tests_state_cmd_abort;
-            wlan_emu_print(wlan_emu_log_level_err,
-                "%s:%d: unable to get stop step number : %d for step : %d\n", __func__, __LINE__,
-                step->u.iperf_client->u.stop_conf.stop_step_number, step->step_number);
-            return RETURN_ERR;
+            // spawn killall iperf3 process
+            std::string iperf_kill_cmd = std::string("killall ") + std::string("/usr/bin/iperf3");
+            if (step->u.iperf_client->interface_type == interface_type_ethernet) {
+                wlan_emu_print(wlan_emu_log_level_dbg, "%s:%d: In ethernet namespace\n", __func__,
+                    __LINE__);
+                ext_emu->enter_namespace("/var/run/netns/ots");
+            }
+            int result = execute_process_once(iperf_kill_cmd,
+                &step->u.iperf_client->u.start_conf.iperf_client_pid, false);
+            if (result != RETURN_OK) {
+                wlan_emu_print(wlan_emu_log_level_err,
+                    "%s:%d: step number : %d iperf kill cmd : %s failed\n", __func__, __LINE__,
+                    step->step_number, iperf_kill_cmd.c_str());
+                step->step_state = wlan_emu_tests_state_cmd_abort;
+                if (step->u.iperf_client->interface_type == interface_type_ethernet) {
+                    ext_emu->leave_namespace();
+                }
+                return RETURN_ERR;
+            }
+            if (step->u.iperf_client->interface_type == interface_type_ethernet) {
+                ext_emu->leave_namespace();
+            }
+            step->step_state = wlan_emu_tests_state_cmd_results;
+            return RETURN_OK;
         }
-
         wlan_emu_print(wlan_emu_log_level_dbg,
             "%s:%d: step number : %d stop step number : %d pid to stop : %d\n", __func__, __LINE__,
             step->step_number, step->u.iperf_client->u.stop_conf.stop_step_number,
