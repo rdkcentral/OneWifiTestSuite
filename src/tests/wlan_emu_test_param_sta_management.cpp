ac14ec0bee2aea8c49fdcff6c09c2389a0f1b7d1
rranga540 <RaviShankar.R_Rangaraj@comcast.com>
2026-06-01 09:13:45 +0000

    Multliclient reconnect

--- src/tests/wlan_emu_test_param_sta_management.cpp
+++ src/tests/wlan_emu_test_param_sta_management.cpp
@@ -873,6 +873,7 @@ int test_step_param_sta_management::step_timeout()
                 if (client_info->is_disconnection_sent == true) {
                     client_info->reconnect_timer++;
                     if (client_info->reconnect_timer >= RECONNECT_TIMEOUT) {
+                        step->m_sim_sta_mgr->clear_interface_data(step->u.sta_test);
                         if (step->m_sim_sta_mgr->reconnect_sta(step->u.sta_test, client_info) ==
                             RETURN_ERR) {
                             wlan_emu_print(wlan_emu_log_level_err,
@@ -1144,7 +1145,6 @@ int test_step_param_sta_management::step_frame_filter(wlan_emu_msg_t *msg)
                         __func__, __LINE__, macaddr, client_macaddr, step->step_number);
                     step->u.sta_test->is_station_associated = false;
                     step->m_sim_sta_mgr->disconnect_sta(step->u.sta_test, client_info);
-                    step->m_sim_sta_mgr->clear_interface_data(step->u.sta_test);
                     return RETURN_UNHANDLED;
                 }
 
